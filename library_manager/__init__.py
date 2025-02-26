9be6932c682cd7bec1a49f576ca6935c8940edb6
YeAxol <16109223+YeAxol@users.noreply.github.com>
2025-02-25 20:15:01 -0500

    new stuff

--- library_manager/__init__.py
+++ library_manager/__init__.py
@@ -1,9 +1,27 @@
-import psycopg 
+import psycopg, asyncio, os
+from dotenv import load_dotenv
+from flask import Flask, g 
 
-from flask import Flask
-
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = Flask(__name__)
+
+async def init_db():
+    return await psycopg.AsyncConnection.connect(DATABASE_URL)
+
+async def close_db(conn):
+    await conn.close()
+
+@app.before_request
+async def before_req():
+    g.db = await init_db()
+
+@app.teardown_request
+async def teardown(exception):
+    db = g.get('db', None)
+    if db is not None:
+        await close_db(db)
 
 @app.route("/")
 def home():
---
+++ library_manager/templates/db.py
@@ -0,0 +1,3 @@
+import asyncio. psycopg
+
+async def   
